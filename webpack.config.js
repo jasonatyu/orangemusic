e379124d636f45f7a2175c66cbdd523db130ef9d
Jason Yu <jason@jasonyu.co>
2019-04-29 19:11:18 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: './frontend/orange_music.jsx',
+    output: {
+        path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'inline-source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+};
