edf7ac34a8dc07f195c6546718a324d774486b27
Jason Yu <jason@jasonyu.co>
2019-05-01 23:17:58 -0700

    Change production configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   config.serve_static_assets = true
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
