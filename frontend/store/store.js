3fa1fdd92f71556947f0003e63564dcd2229fe1c
Jason Yu <jason@jasonyu.co>
2019-05-03 09:07:27 -0700

    Fix session errors styling

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,7 +4,15 @@ import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
-    return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+    let middleware = [thunk];
+    if (process.env.NODE_ENV !== 'production') {
+        middleware = [...middleware, logger];
+    }
+    return createStore(
+        rootReducer,
+        preloadedState,
+        applyMiddleware(...middleware)
+    );
 };
 
 export default configureStore;
