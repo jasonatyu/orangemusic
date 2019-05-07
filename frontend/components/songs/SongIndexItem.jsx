dd4d17b7830d5c17dbccdcba288f5c7df6410d2e
Jason Yu <jason@jasonyu.co>
2019-05-06 19:00:19 -0700

    Fix hover

--- frontend/components/songs/SongIndexItem.jsx
+++ frontend/components/songs/SongIndexItem.jsx
@@ -5,12 +5,22 @@ class SongIndexItem extends React.Component {
     
     constructor(props) {
         super(props);
-        this.state= {menuVisible: false};
+        this.state= {menuVisible: false, hoverSong: false};
         this.handleClick = this.handleClick.bind(this);
+        this.handleMouseEnter = this.handleMouseEnter.bind(this);
+        this.handleMouseLeave = this.handleMouseLeave.bind(this);
     }
 
     handleClick(e) {
         this.setState({menuVisible: !this.state.menuVisible});
+    }
+
+    handleMouseEnter(e){
+        this.setState({ hoverSong: true });
+    }
+
+    handleMouseLeave(e) {
+        this.setState({ hoverSong: false });
     }
 
     secondsToMinutes(time) {
@@ -29,14 +39,15 @@ class SongIndexItem extends React.Component {
             title = `${song.title}`;
         }
         return (
-            <tr key={song.id} className={"songs-body" + (this.props.display === "album" ? " display-album" : " display-other") }>
+            <tr onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} key={song.id} className={"songs-body" + (this.props.display === "album" ? " display-album" : " display-other") }>
                 {display === 'album' ? "" : <td><img src={song.photoUrl} width="40"/></td>}
                 {display === 'album' ? <td className='content'>{idx+1}</td> : ""}
                 <td className='content'><p>{title}</p>{display === 'album' ? "":<p className='song-album'>{song.album}</p>}</td>
                 <td className='content'>{song.artist}</td> 
                 {display === 'album' ? "" : <td className='content'>{song.year}</td> }          
                 <td className='content last-item' onClick={this.handleClick}>
-                    <span className='hide-hover'>{this.secondsToMinutes(song.time)}</span><span className='show-hover'>•••</span>
+                    {this.state.hoverSong ? <span id='show-hover'>•••</span> : <span>{this.secondsToMinutes(song.time)}</span>}
+                    {/* <span className={this.state.hoverSong ? 'hide-hover' : ''}>{this.secondsToMinutes(song.time)}</span><span className={this.state.hoverSong ? 'show-hover' : ''}>•••</span> */}
                     {this.state.menuVisible ? < SongMenuContainer optionClicked={this.handleClick} song={song} display={this.props.display} /> : ""}
                 </td>
             </tr>
