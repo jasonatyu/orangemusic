b15c6e853b44141dac3d77fa18122c4769e46c85
Jason Yu <jason@jasonyu.co>
2019-05-08 12:06:42 -0700

    Add search bar styling

--- frontend/components/auth_navigation/AuthMainNavBar.jsx
+++ frontend/components/auth_navigation/AuthMainNavBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import AudioPlayerContainer from '../audio_player/AudioPlayerContainer';
+import SearchBar from '../search_bar/SearchBar';
 
 const AuthMainNavBar = () => (
-    <nav id='auth-main-nav-bar'>
-        <AudioPlayerContainer />
-    </nav>
+    <div id='auth-main-nav-div'>
+        <nav id='auth-main-nav-bar'>
+            <AudioPlayerContainer />
+        </nav>
+        <SearchBar />
+    </div>
 )
 
 export default AuthMainNavBar;
---
+++ frontend/components/search_bar/SearchBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBar = () => (
+    <div id='search-bar-div'>
+        <span><i class="fas fa-search"></i></span><input type="text" id='search-bar' placeholder="Search"/>
+    </div>
+);
+
+export default SearchBar;
